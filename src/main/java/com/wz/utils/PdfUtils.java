bc418763371a6da950f853230077a8a368a0ee8f
王青玄 <1121586359@qq.com>
2023-05-11 23:09:31 +0800

    pdf代码更改

--- src/main/java/com/wz/utils/PdfUtils.java
+++ src/main/java/com/wz/utils/PdfUtils.java
@@ -7,6 +7,7 @@ import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
@@ -52,6 +53,47 @@ public class PdfUtils {
 
         // 输出Word文件
         String fileName = pdfFile.getOriginalFilename().replace(".pdf", ".docx");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", fileName);
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+        OutputStream out = response.getOutputStream();
+        document.write(out);
+        out.close();
+        document.close();
+    }
+
+    /**
+     * pdf转成word
+     * @param pdfFile
+     * @param outputFileName
+     * @param response
+     * @throws IOException
+     */
+    public static void pdfToWord(MultipartFile pdfFile, String outputFileName, HttpServletResponse response) throws IOException {
+
+        // 读取PDF文件内容
+        PDDocument pdf = PDDocument.load(pdfFile.getInputStream());
+        PDFTextStripper stripper = new PDFTextStripper();
+        String text = stripper.getText(pdf);
+        pdf.close();
+
+        // 将PDF内容写入Word文档
+        XWPFDocument document = new XWPFDocument();
+        String[] textPerPage = text.split("\u000C"); // 根据换页符拆分文本
+        for (int i = 0; i < textPerPage.length; i++) {
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun run = paragraph.createRun();
+            run.setText(textPerPage[i]);
+            if (i < textPerPage.length - 1) {
+                // 添加分页符
+                run.addBreak();
+                run.addBreak();
+            }
+        }
+
+        // 输出Word文件
+        String fileName = outputFileName.endsWith(".docx") ? outputFileName : outputFileName + ".docx";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         headers.setContentDispositionFormData("attachment", fileName);
